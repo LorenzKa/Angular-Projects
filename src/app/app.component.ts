fa3251263900384f319d3452aa070e65cc62dade
Lorenz <lorenz.kassewalder95@gmail.com>
2021-09-20 18:26:36 +0200

    Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 
-interface Animal {
-  name: string;
-  legs: number;
-}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,29 +7,4 @@ interface Animal {
 })
 export class AppComponent {
   title = 'My Angular Demo';
-  showErrors = true;
-  errorMessage = 'Bitte Vorname eintragen';
-  userInput = '';
-  userSelection = 'A';
-  animals: Animal[] = [
-    {
-      name: 'Dog',
-      legs: 4,
-    },
-    {
-      name: 'Snake',
-      legs: 0,
-    },
-    {
-      name: 'Bird',
-      legs: 2,
-    },
-  ];
-  selectedAnimal: Animal = this.animals[0];
-  options = ['First Entry', 'Second Entry', 'Third Entry'];
-
-  setErrorMsg(msg: string): void {
-    this.showErrors = false;
-    this.errorMessage = msg;
-  }
 }
