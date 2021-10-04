fb92a4e4d3831349abcee297bb155ed4837c8c96
Lorenz <lorenz.kassewalder95@gmail.com>
2021-10-04 15:59:17 +0200

    Final Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
+import { HttpClient } from '@angular/common/http'
 import { Component } from '@angular/core';
+interface ICustomer {
+  id: number;
+  firstname: string;
+  lastname: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My Angular Demo';
+  formCustomerId : string = ''
+  formFirstname : string = ''
+  formLastname : string = ''
+  customers: ICustomer[] = [];
+  customerId: number = 1
+  constructor(private http: HttpClient) {}
+  getAllCustomers():void {
+    this.http.get<ICustomer[]>('http://localhost:8000/api/customers').subscribe(result => this.customers = result)
+  }
+  getCustomer():void{
+    this.http.get<ICustomer>('http://localhost:8000/api/customers/'+this.customerId).subscribe(result => this.customers.push(result))
+  }
+  //TODO: Not Finished
+  createCustomer():void{
+    this.http.post('http://localhost:8000/api/customers', this.formFirstname)
+  }
 }
