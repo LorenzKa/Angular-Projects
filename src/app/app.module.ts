ffed18c34b981e2cbe28f38db23c6bbe96f11663
LorenzKa <lorenz.kassewalder95@gmail.com>
2021-10-20 17:33:16 +0200

    Added material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSliderModule } from '@angular/material/slider';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, BrowserAnimationsModule, MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
