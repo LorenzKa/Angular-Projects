d8f2d72b2f9aa5ad2985f8292d9e7da745d8a6d6
Lorenz <lorenz.kassewalder95@gmail.com>
2021-10-22 08:16:04 +0200

    Improved Design and Functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,10 @@ interface IMatchList{
   roundNumber: number,
   matches: IMatch[]
 }
+interface IWinnerResponse{
+  matchId: number,
+  winner: number
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -27,11 +31,54 @@ export class AppComponent {
   roundNumber: number = 0;
   title = 'My Angular Demo';
   baseurl = "http://localhost:5000/api/"
+  buttonValue = ""
   ngOnInit(): void{
-    this.getMatchesWithoutWinner();
+    this.getMatchesFromCurrentRound();
   }
   constructor(private http: HttpClient) {}
-  getMatchesWithoutWinner() : void{
-    this.http.get<IMatchList>("http://localhost:5000/api/Match/WihoutWinner").subscribe(result => this.matchList = result)
+  getMatchesFromCurrentRound() : void{
+    this.http.get<IMatchList>(this.baseurl+"Match/CurrentRound").subscribe(result => this.matchList = result)
+  }
+  setWinner(playerId : number, matchId: number): void{
+    console.log("playerid:"+playerId+" matchid:"+matchId)
+    
+    this.http.post<IWinnerResponse>(this.baseurl+"Match", { playerid: playerId, matchid: matchId } ).subscribe(result => {
+      const winnerResponse : IWinnerResponse = result
+      console.log(winnerResponse)
+      console.log(this.matchList.matches.find(x => x.id == winnerResponse.matchId))
+      this.matchList.matches.find(x => x.id == winnerResponse.matchId)!.winner=winnerResponse.winner;
+      
+    })
+  }
+  startTournament(){
+    this.http.delete(this.baseurl+"Match").subscribe(result => console.log(result))
+    this.http.get<IMatchList>(this.baseurl+"Match/GenerateRound").subscribe(x => this.matchList = x)
+  }
+  nextRound(){
+    this.http.get<IMatchList>(this.baseurl+"Match/GenerateRound").subscribe(x => this.matchList = x)
+  }
+  isWinner(playerNumber: number, matchId: number) : string{
+    if(this.matchList.matches.find(x => x.id == matchId)?.winner == playerNumber){
+      return 'primary'
+    }
+    else{
+      return 'warn'
+    }
+  }
+  hasMatches(): string{
+    if(this.matchList.matches.length > 1){
+      return 'accent'
+    }
+    else{
+      return 'primary'
+    }
+  }
+  canContinue(): boolean{
+    if(this.matchList.matches.filter(x => x.winner == null).length == 0 && this.matchList != undefined){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 }
